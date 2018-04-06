452ae4a96afd41246c8f02abe053aca99dc87696
Lucas Alegre <lucasnale@gmail.com>
2018-04-05 21:18:50 -0300

    cidentify and ccreate

--- src/cthread.c
+++ src/cthread.c
@@ -1,5 +1,6 @@
-#include <stdin.h>
+#include <stdio.h>
 #include <ucontext.h>
+#include <string.h>
 #include "cdata.h"
 #include "support.h"
 #include "cthread.h"
@@ -9,7 +10,9 @@ FILA2 blockedQueue;
 FILA2 sRunQueue;
 FILA2 sBlockedQueue;
 
-TCB_t *running;
+TCB_t mainThread;
+
+TCB_t *runningThread;
 
 ucontext_t scheduler;
 
@@ -22,22 +25,23 @@ int getNextTid(){
 
 void initializeCthread(){
 	if(libraryInitialized)
-		rerturn;
+		return;
 
 	if(CreateFila2(&runQueue) != 0)
-		printf("Erro na criação da fila de aptos\n");
-	if(CreateFila2(&srunQueue) != 0)
-		printf("Erro na criação da fila de apto-suspenso\n");
+		printf("Error: Run Queue initialization failed\n");
+	if(CreateFila2(&sRunQueue) != 0)
+		printf("Error: Suspend-Run Queue initialization failed\n");
 	if(CreateFila2(&blockedQueue) != 0)
-		printf("Erro na criação da fila de bloqueado\n");
+		printf("Error: Blocked Queue initialization failed\n");
 	if(CreateFila2(&sBlockedQueue) != 0)
-		printf("Erro na criação da fila de bloqueado-suspenso\n");
+		printf("Error: Suspend-Blocked Queue initialization failed\n");
 	
-	TCB_t mainThread;
 	mainThread.tid = 0;
 	mainThread.prio = 0;
 	mainThread.state = PROCST_EXEC;
 	getcontext(&(mainThread.context));
+
+    runningThread = &mainThread;
 
 	getcontext(&scheduler);
 	makecontext(&scheduler, FUNCAOAQUI, 0);
@@ -45,94 +49,52 @@ void initializeCthread(){
 	libraryInitialized = 1;
 }
 
-/******************************************************************************
-Parâmetros:
-	name:	ponteiro para uma área de memória onde deve ser escrito um string que contém os nomes dos componentes do grupo e seus números de cartão.
-		Deve ser uma linha por componente.
-	size:	quantidade máxima de caracteres que podem ser copiados para o string de identificação dos componentes do grupo.
-Retorno:
-	Se correto => 0 (zero)
-	Se erro	   => Valor negativo.
-******************************************************************************/
-int cidentify (char *name, int size);
+int cidentify (char *name, int size){
 
-/******************************************************************************
-Parâmetros:
-	start:	ponteiro para a função que a thread executará.
-	arg:	um parâmetro que pode ser passado para a thread na sua criação.
-	prio:	NÃO utilizado neste semestre, deve ser sempre zero.
-Retorno:
-	Se correto => Valor positivo, que representa o identificador da thread criada
-	Se erro	   => Valor negativo.
-******************************************************************************/
-int ccreate (void* (*start)(void*), void *arg, int prio){
+    char *group = "Lucas Nunes Alegre 00274693\nAline Weber\nLucas Sonntag Hagen\n\0";
+    if(size < strlen(group){
+        printf("Size given is not sufficient to copy the whole string!\n");
+        return -1;
+    }
 
+    strncpy(name, group, size-1);
+    name[size] = '\0';
 
-	
-
-
+    return 0;
 }
 
-/******************************************************************************
-Parâmetros:
-	Sem parâmetros
-Retorno:
-	Se correto => 0 (zero)
-	Se erro	   => Valor negativo.
-******************************************************************************/
+int ccreate (void* (*start)(void*), void *arg, int prio){
+    initializeCthread();
+	
+    TCB_t *createdThread = (*TCB_t)malloc(sizeof(TCB_t));
+    createdThread->tid = getNextTid();   
+    createdThread->prio = 0;
+    createdThread->state = PROCST_APTO;
+    
+    getcontext(&(createdThread->context));
+    createdThread->context.uc_link = //linkarr;
+    createdThread->context.uc_stack.ss_sp = (char*)malloc(SIGSTKSZ);
+    createdThread->context.uc_stack.ss_size = SIGSTKSZ;
+    makecontext(&(createdThread->context), (void(*)void)start, 1, arg);
+
+    if(AppendFila2(&runQueue, (void*)createdThread) != 0){
+        printf("Error: insertion of the new thread in the Run Queue failed\n");
+        return -1;
+    }
+
+    return 0;    
+}
+
 int cyield(void);
 
-/******************************************************************************
-Parâmetros:
-	tid:	identificador da thread cujo término está sendo aguardado.
-Retorno:
-	Se correto => 0 (zero)
-	Se erro	   => Valor negativo.
-******************************************************************************/
 int cjoin(int tid);
 
-/******************************************************************************
-Parâmetros:
-	tid:	identificador da thread a ser suspensa.
-Retorno:
-	Se correto => 0 (zero)
-	Se erro	   => Valor negativo.
-******************************************************************************/
 int csuspend(int tid);
 
-/******************************************************************************
-Parâmetros:
-	tid:	identificador da thread que terá sua execução retomada.
-Retorno:
-	Se correto => 0 (zero)
-	Se erro	   => Valor negativo.
-******************************************************************************/
 int cresume(int tid);
 
-/******************************************************************************
-Parâmetros:
-	sem:	ponteiro para uma variável do tipo csem_t. Aponta para uma estrutura de dados que representa a variável semáforo.
-	count: valor a ser usado na inicialização do semáforo. Representa a quantidade de recursos controlados pelo semáforo.
-Retorno:
-	Se correto => 0 (zero)
-	Se erro	   => Valor negativo.
-******************************************************************************/
 int csem_init(csem_t *sem, int count);
 
-/******************************************************************************
-Parâmetros:
-	sem:	ponteiro para uma variável do tipo semáforo.
-Retorno:
-	Se correto => 0 (zero)
-	Se erro	   => Valor negativo.
-******************************************************************************/
 int cwait(csem_t *sem);
 
-/******************************************************************************
-Parâmetros:
-	sem:	ponteiro para uma variável do tipo semáforo.
-Retorno:
-	Se correto => 0 (zero)
-	Se erro	   => Valor negativo.
-******************************************************************************/
 int csignal(csem_t *sem);
