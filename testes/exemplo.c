1068031c3f1cfab11bf959f0dd8ebccc62660cd9
Lucas Alegre <lucasnale@gmail.com>
2018-04-15 20:09:52 -0300

    Testes folder created

---
+++ testes/exemplo.c
@@ -0,0 +1,39 @@
+
+/*
+ *	Programa de exemplo de uso da biblioteca cthread
+ *
+ *	Versão 1.0 - 14/04/2016
+ *
+ *	Sistemas Operacionais I - www.inf.ufrgs.br
+ *
+ */
+
+#include "../include/support.h"
+#include "../include/cthread.h"
+#include <stdio.h>
+
+void* func1(void *arg) {
+    cjoin(*((int*)arg) + 1);
+
+	printf("eu sou a thread imprimindo %d\n", *((int *)arg));
+}
+
+int main(int argc, char *argv[]) {
+
+	int	id[10];
+	int i, num[10];
+
+    for(i=0;i<10;i++){
+	    num[i] = i+1;
+    }
+
+    for(i=0;i<10;i++){
+	    id[i] = ccreate(func1, (void *)&num[i], 0);
+    }
+
+    cjoin(1);
+
+
+	printf("Eu sou a main voltando para terminar o programa\n");
+}
+
