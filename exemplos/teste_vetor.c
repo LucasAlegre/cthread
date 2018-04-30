69d2ae7f387644172259144078619e1edc366e0a
Lucas Hagen <lucas_hagen@hotmail.com>
2018-04-30 20:07:20 -0300

    Final changes

--- exemplos/teste_vetor.c
+++ exemplos/teste_vetor.c
@@ -36,18 +36,18 @@ int main(int argc, char *argv[]) {
   
     for (i = 0; i < MAX_THR; i++) {
         pid[i] = ccreate(func, (void *)('A'+i), 0);
-       if ( pid[i] == -1) {
-          printf("ERRO: criação de thread!\n");
-          exit(-1);
-       }
-     }
+        if ( pid[i] == -1) {
+            printf("ERRO: criação de thread!\n");
+            exit(-1);
+        }
+    }
 
     for (i = 0; i < MAX_THR; i++) 
          cjoin(pid[i]);
 
     for (i = 0; i < MAX_SIZE; i++) {    
         if ( (i % 20) == 0 )
-           printf("\n");
+            printf("\n");
         printf("%c", (char)vetor[i]);
     }
       
--- teste/teste2.c
+++ teste/teste2.c
@@ -3,82 +3,43 @@
 
 #include "cthread.h"
 
-void sumValue();
-void subValue();
-void divValue();
-void multValue();
-void absValue();
-void powValue();
-csem_t sem;
+void func1();
+void func2();
 
-int th1, th2, th3, th4;
-int value = 10;
+int th1, th2;
 
+void func1() {
+	printf("[%d]: START\n", th1);
 
-void sumValue(){
-    value = value + 5;
-    printf("+ 5 = %d - cyield\n", value);
-    cyield();
-    value = value + 3;
-    printf("+ 3 = %d - cyield\n", value);
-    cyield();
-    value = value + 5;
-    printf("+ 5 = %d - cjoin(div) - won't join\n", value);
-    cjoin(th4);
-    value = value + 1;
-    printf("+ 1 = %d - end sum thread\n", value);
-}
-void subValue(){
-    value = value - 4;
-    printf("- 4 = %d - cwait - continue\n", value);
-    cwait(&sem);
-    value = value - 1;
-    printf("- 1 = %d - cyield\n", value);
-    cyield();
-    value = value - 9;
-    printf("- 9 = %d - cyield\n", value);
-    cyield();
-    printf("sub made a csignal and cjoin(sum)\n");
-    csignal(&sem);
-    cjoin(th1);
-    value = value - 11;
-    printf("- 5 = %d - end sub thread\n", value);
-}
-void divValue(){
-    value = (int)value/2;
-    printf("/ 2 = %d - cwait -> BLOCKED\n", value);
-    cwait(&sem);
-    value = (int)value/4;
-    printf("/ 4 = %d - end div thread\n", value);
-}
-void multValue(){
-    value = value*3;
-    printf("* 3 = %d - cjoin(div)\n", value);
-    cjoin(th3);
-    value = value*5;
-    printf("* 5 = %d - end mult thread\n", value);
+	cjoin(th2);
 
+	printf("[%d]: STOP\n", th1);
 }
 
+void func2() {
+	printf("[%d]: START\n", th2);
+
+	csuspend(th1);
+
+	printf("[%d]: STOP\n", th2);
+}
 
 int main(){
-    csem_init(&sem, 1);
+	th1 = ccreate((void*) func1, (void*) NULL, 0);
+	printf("[MAIN] Thread with pid = %d\n", th1);
 
-    th1 = ccreate((void *)sumValue, (void *)NULL, 0);
-	printf("creating sum thread with tid = %d\n", th1);
+	th2 = ccreate((void*) func2, (void*) NULL, 0);
+	printf("[MAIN] Thread with pid = %d\n", th2);
 
-    th2 = ccreate((void *)subValue, (void *)NULL, 0);
-	printf("creating sub thread with tid = %d\n", th2);
+	printf("[MAIN] CYIELD\n");
+	cyield();
+	
+	printf("[MAIN] CRESUME %d\n", th1);
+	cresume(th1);
 
-    th3 = ccreate((void *)divValue, (void *)NULL, 0);
-	printf("creating div thread with tid = %d\n", th3);
+	printf("[MAIN] CJOIN %d\n", th1);
+	cjoin(th1);
 
-    th4 = ccreate((void *)multValue, (void *)NULL, 0);
-	printf("creating mult thread with tid = %d\n", th4);
-
-	printf("initial value = %d\n", value);
-
-    cjoin(th2);
-    printf("value = %d\n", value);
+	printf("[MAIN] STOPPED\n");
 	return 0;
 }
